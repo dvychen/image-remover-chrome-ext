78ca43b1ac8fb0e2a6d7f459e98b06fde49355c2
David Chen <dvychen@gmail.com>
2020-12-31 02:29:29 -0500

    Fixed typo resulting in bug.

--- Scripts/remove-imgs.js
+++ Scripts/remove-imgs.js
@@ -2,5 +2,5 @@ var listImgs = document.getElementsByTagName("img");
 
 var i;
 for (i = 0; i < listImgs.length; i++) {
-    listImgs[i].className += "remove-opacity"
+    listImgs[i].className += " remove-opacity"
 }
